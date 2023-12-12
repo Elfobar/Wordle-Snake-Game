b3eb2aa9e1ea207647ff0e2b958cf95db8b9d077
Maksym Matsuhyria <maksymm@chalmers.se>
2023-12-12 14:52:44 +0100

    Added the word at the top of the screen

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,10 +1,21 @@
 package com.example.demo;
 
 public class AppConfig {
-    private static final String WORDS_RELATIVE_PATH = "/file_input/words.txt";
+    private static final String WORDS_RELATIVE_PATH = "/src/main/resources//file_input/words.txt";
+    private static final String FONT_RELATIVE_PATH = "/custom_font/blade.ttf";
+    private static final int FONT_SIZE = 60;
 
-    public static String getWordsPathFile(){
+    public static String getWordsPathFile() {
         String currentWorkingDir = System.getProperty("user.dir");
         return currentWorkingDir + WORDS_RELATIVE_PATH;
     }
+
+    public static String getFontPathFile(){
+        return FONT_RELATIVE_PATH;
+    }
+
+    public static int getFontSize(){
+        return FONT_SIZE;
+    }
+
 }
